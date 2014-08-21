0972d3c08bfc3bc97310c62dcf7bd9f2804897fc
Niels Vermaut <nielsvermaut@gmail.com>
2014-08-21 17:41:11 +0200

    Tweaks and improvements

--- search/js/widgets/ResultWidget.js
+++ search/js/widgets/ResultWidget.js
@@ -1,23 +1,46 @@
 (function ($){
     AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
 
+        start: 0,
+
+        beforeRequest: function () {
+            $(this.target).html($('<img>').attr('src', 'images/ajax-loader.gif'));
+        },
+
+        facetLinks: function (facet_field, facet_values) {
+            var links = [];
+            if (facet_values) {
+                for (var i = 0, l = facet_values.length; i < l; i++) {
+                    if (facet_values[i] !== undefined) {
+                        links.push(
+                            $('<a href="#"></a>')
+                                .text(facet_values[i])
+                                .click(this.facetHandler(facet_field, facet_values[i]))
+                        );
+                    }
+                    else {
+                        links.push('Niet beschikbaar met deze selectie');
+                    }
+                }
+            }
+            return links;
+        },
+
+        facetHandler: function (facet_field, facet_value) {
+            var self = this;
+            return function () {
+                self.manager.store.remove('fq');
+                self.manager.store.addByValue('fq', facet_field + ':' + AjaxSolr.Parameter.escapeValue(facet_value));
+                self.doRequest();
+                return false;
+            };
+        },
+
         afterRequest: function(){
             $(this.target).empty();
 
             for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++){
                 var doc = this.manager.response.response.docs[i];
-
-                var items = [];
-                items = items.concat(this.facetLinks(doc.entity_type));
-                items = items.concat(this.facetLinks(doc.ss_name));
-
-                var $links = $('#links_' + doc.id);
-
-                $links.empty();
-
-                for(var j = 0, m = items.length; j < m; j++){
-                    $links.append($('<li></li>').append(items[j]));
-                }
 
                 $(this.target).append(this.template(doc));
             }
@@ -27,43 +50,18 @@
             var snippet = '';
 
             if(doc.teaser.length > 300){
-                snippet += doc.ds_created + ' ' + doc.teaser.substring(0,300);
+                snippet += '<div class="row"><div class="col-md-9">' + doc.teaser.substring(0,300) + '</div><div class="col-md-3 right">'+ doc.ds_created +'</div></div>';
                 snippet += '<span style="display: none">' + doc.teaser.substring(300) + '</span><a href="#" class="more">Meer</a>';
             } else {
-                snippet += doc.ds_created + ' ' + doc.teaser;
+                snippet += '<div class="row"><div class="col-md-9">' + doc.teaser + '</div><div class="col-md-3 right">'+ doc.ds_created +'</div></div>';
             }
 
             var output = '';
-            output += '<div class="result"><h3>' + doc.label +'</h3></div>';
-            output += '<p id="links_' + doc.id + '" class="links"></p>';
+            output += '<div class="result"><a href="' + doc.path + '"><h3>' + doc.label +'</h3></a></div>';
+            output += '<ul id="links_' + encodeURIComponent(doc.id) + '" class="links"></ul>';
             output += '<p>' + snippet +'</p>'
 
             return output;
-        },
-
-        facetLinks: function(facet_field, facet_values){
-
-            var links = [];
-
-            if(facet_values){
-                for(var i = 0, l = facet_values.length; i < l; i++){
-                    links.push($('<a href="#"></a>').text(facet_values[i]).click(this.facetHandler(facet_field, facet_values[i])));
-                }
-            }
-
-            return links;
-
-        },
-
-        facetHandler: function (facet_field, facet_value){
-            var self = this;
-
-            return function(){
-                self.manager.store.remove('fq');
-                self.manager.store.addByValue('fq', facet_field + ':' + AjaxSolr.Parameter.escapeValue(facet_value));
-                self.doRequest();
-                return false;
-            };
         },
 
         init: function(){
