97085ebe464bc0d5d9fa8d9b40371288f70a95cf
Niels Vermaut <nielsvermaut@gmail.com>
2014-08-21 17:39:32 +0200

    Added widgets to index

--- search/js/westtoer.js
+++ search/js/westtoer.js
@@ -3,7 +3,7 @@ var Manager;
 (function($){
     $(function (){
         Manager = new AjaxSolr.Manager({
-            solrUrl: 'http://192.168.0.98:8983/solr/'
+            solrUrl: 'http://search.westtoer.be:8983/solr/'
         });
         //Define the widgets
 
@@ -37,10 +37,49 @@ var Manager;
             id: 'text',
             target: '#search'
         }));
+
+        /*var facetTypes = ['ss_name', 'entity_type', 'ds_created', 'content_type'];
+        var facetTranslations = ['Auteur', 'Entiteit Type', 'Datum', 'Bestandstype'];
+        for (var i = 0, l = facetTypes.length; i < l; i++){
+            Manager.addWidget(new AjaxSolr.FacetWidget({
+                id: facetTypes[i],
+                target: '#'. facerTypes[i],
+                field: fields[i],
+                translation: facetTranslations[i]
+            }));
+        }*/
+
+        var fields = [ 'ss_name', 'content_type', 'entity_type' ];
+        var translations = ['Auteur', 'Bestandstype', 'Entiteitstype']
+        for (var i = 0, l = fields.length; i < l; i++) {
+            Manager.addWidget(new AjaxSolr.FacetWidget({
+                id: fields[i],
+                target: '#' + fields[i],
+                field: fields[i],
+                translation: translations[i]
+            }));
+        }
+
+        /*Manager.addWidget(new AjaxSolr.TextWidget({
+            id: 'text',
+            target: '#search',
+            fields: ['ss_name', 'label', 'entity_type']
+        }));*/
+
         Manager.init();
+        var params = {
+            facet: true,
+            'facet.field': [ 'ss_name', 'entity_type', 'ds_created', 'content_type'],
+            'facet.mincount': 0,
+            'f.ss_name.facet.limit': 50,
+            'json.nl': 'map'
+        };
+        for (var name in params) {
+            Manager.store.addByValue(name, params[name]);
+        }
+
         Manager.store.addByValue('q', '*:*');
         Manager.doRequest();
-
 
 
     });
